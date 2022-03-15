3f0ce54ac0a826b87f4f9d325a01921b53987ea4
wweidler <weidlerwill@gmail.com>
2022-03-14 21:35:31 -0500

    Fixed warning with mixing int states

--- finding.cpp
+++ finding.cpp
@@ -5,10 +5,11 @@ using namespace std;
 /***********************FUNCTION DEFINITIONS***********************/
 
 int findCriminal(string array[], int SIZE){
-    long min = array[0].length();
+    int min = array[0].length();
     int returnValue = 0;
     for (int i=1; i<SIZE; i++){
-        if(array[i].length() > min){
+        int names = array[i].length();
+        if(names > min){
             returnValue = i;
         }
     }
