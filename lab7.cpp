0032a78a29c8e1e8fd75f653d8a305c228479c71
wweidler <weidlerwill@gmail.com>
2022-03-14 21:02:46 -0500

    Created file, transfer to work on desktop

---
+++ lab7.cpp
@@ -0,0 +1,22 @@
+// Programmer: Will Weidler
+// ID: wawq97
+// Date: 2/28/22
+// File: hw4.cpp
+// Assignment: HW4
+// Purpose: This program outputs the summary of 3 different slimes
+
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+    int i;
+    string arrayInput;
+    string arrayNames[5];
+    for(i=0, i<5, i++){
+        cin >> arrayInput;
+
+
+    }
+}
